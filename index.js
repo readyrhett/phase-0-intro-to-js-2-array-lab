a14e9298491c97e3e766d9cefbf9fc4ed5c45e54
Rhett Chase <rhettchase@yahoo.com>
2023-03-30 15:12:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() 
+{
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat()
+{
+    cats.unshift('Bob')
+    return cats;
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+
+function appendCat()
+{
+   return [...cats,'Broom'];
+}
+
+function prependCat() {
+    return ['Arnold',...cats]
+}
+
+function removeLastCat() {
+   return cats.slice(0,-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
